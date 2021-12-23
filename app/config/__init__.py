3d38a8e3bf60462a8771e7716507e2661caba5d9
Elizaveta <sms-elizabet@mail.ru>
2021-12-23 22:11:20 +0300

    new

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,6 +1,10 @@
+import os
+
+
 class Config:
     SECRET_KEY = "top secret"
     JWT_ACCESS_LIFESPAN = {"hours": 24}
     JWT_REFRESH_LIFESPAN = {"days": 30}
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace('postgres:', 'postgresql:')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
