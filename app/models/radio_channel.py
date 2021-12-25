5d67d3677515942632a51a986e7295ba4c4331b7
Elizaveta <sms-elizabet@mail.ru>
2021-12-25 20:32:39 +0300

    new change radio_channel

--- app/models/radio_channel.py
+++ app/models/radio_channel.py
@@ -8,3 +8,22 @@ class RadioChannel(db.Model):
     radio_stream_url = db.Column(db.String(1000))
     is_active = db.Column(db.Boolean, default=True)
     is_popular = db.Column(db.Boolean, default=True)
+
+    @property
+    def identity(self):
+        """
+        *Required Attribute or Property*
+        flask-praetorian requires that the user class has an ``identity`` instance
+        attribute or property that provides the unique id of the user instance
+        """
+        return self.id
+
+    @classmethod
+    def identify(cls, radio_id):
+        """
+        *Required Method*
+        flask-praetorian requires that the user class implements an ``identify()``
+        class method that takes a single ``id`` argument and returns user instance if
+        there is one that matches or ``None`` if there is not.
+        """
+        return db.session.query(cls).get(radio_id)
--- app/resource/radio_channel.py
+++ app/resource/radio_channel.py
@@ -7,6 +7,7 @@ from flask_restx import Namespace, Resource
 from app.models import RadioChannel
 from app.models.db_init import db
 from app.schema.radio_channel import RadioChannelSchema
+from app.resource.init_guard import guard
 
 radio_channel_ns = Namespace('radio_channel', description='Операции для взаимодействия с постами')
 
@@ -26,3 +27,26 @@ class RadioChannelResource(Resource):
         db.session.add(radio_channel)
         db.session.commit()
         return {'status': 'ok'}
+
+
+@radio_channel_ns.route("/<int:radio_id>")
+class RadioChannelChangeResource(Resource):
+    @flask_praetorian.auth_required
+    @radio_channel_ns.doc('Radio data', security='Bearer')
+    @responds(schema=RadioChannelSchema, api=radio_channel_ns, status_code=200)
+    def get(self, radio_id):
+        return db.session.query(RadioChannel).get(radio_id)
+
+    @flask_praetorian.auth_required
+    @radio_channel_ns.doc('Radio editing', security='Bearer')
+    @accepts(schema=RadioChannelSchema, api=radio_channel_ns)
+    @responds(schema=RadioChannelSchema, api=radio_channel_ns, status_code=200)
+    def put(self, radio_id):
+        radio = request.parsed_obj
+        if radio.id is not None:
+            radio.id = guard.extract_jwt_token(guard.read_token())['id']
+        if radio_id.id != guard.extract_jwt_token(guard.read_token())['id']:
+            return {'status': 'error', 'message': 'Permission denied'}, 403
+        db.session.add(radio)
+        db.session.commit()
+        return radio
