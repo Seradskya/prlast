7124bced68aa186c8e7916ee775ecd0188b08f36
Elizaveta <sms-elizabet@mail.ru>
2022-04-16 23:44:30 +0300

    add playlist, mobile_user and functions

--- app/models/association_table.py
+++ app/models/association_table.py
@@ -1,6 +1,6 @@
 from .db_init import db
 
 association_table = db.Table('playlist_to_channel',
-     db.Column('playlist', db.Integer, db.ForeignKey('playlist.id'), primary_key=True),
+    db.Column('playlist', db.Integer, db.ForeignKey('playlist.id'), primary_key=True),
     db.Column('radio_channel', db.Integer, db.ForeignKey('radio_channel.id'), primary_key=True)
 )
