938434ece0bcc66644ac35932f2d7c4be7057e80
Elizaveta <sms-elizabet@mail.ru>
2022-05-14 12:37:28 +0300

    add abtests

--- app/resource/percent.py
+++ app/resource/percent.py
@@ -57,9 +57,9 @@ class PercentRandomResource(Resource):
     def get(self):
         probability_first = db.session.query(Percent).filter_by(Percent.scen_id == 1).first()
         probability_second = db.session.query(Percent).filter_by(Percent.scen_id == 2).first()
-        result = random.choices([1, 2], weights=[probability_first.probability/100, probability_second.probability/100])[0]
+        #result = random.choices([1, 2], weights=[probability_first.probability/100, probability_second.probability/100])[0]
 
-        return {'scen_id': result}
+        return {'scen_id': probability_first}
 
 
 
